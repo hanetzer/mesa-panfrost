67d34ae40c5c042cbf12bc95235412f2da9e770e
Alyssa Rosenzweig <alyssa@rosenzweig.io>
2018-01-24 20:36:25 +0000

    Import midgard shader defines by Connor Abbott
    These were found in the original Midgard disassemble by cwabbott,
    extracted from the project cwabbots-open-gpu-tools under the license
    stated. They will be used here for instruction emission in the Midgard
    compiler.
    
    Signed-off-by: Alyssa Rosenzweig <alyssa@rosenzweig.io>

---
+++ src/gallium/drivers/panfrost/midgard/midgard.h
@@ -0,0 +1,205 @@
+/* Author(s):
+ *   Connor Abbott
+ *   Alyssa Rosenzweig
+ *
+ * Copyright (c) 2013 Connor Abbott (connor@abbott.cx)
+ * Copyright (c) 2018 Alyssa Rosenzweig (alyssa@rosenzweig.io)
+ *
+ * Permission is hereby granted, free of charge, to any person obtaining a copy
+ * of this software and associated documentation files (the "Software"), to deal
+ * in the Software without restriction, including without limitation the rights
+ * to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+ * copies of the Software, and to permit persons to whom the Software is
+ * furnished to do so, subject to the following conditions:
+ *
+ * The above copyright notice and this permission notice shall be included in
+ * all copies or substantial portions of the Software.
+ *
+ * THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+ * IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+ * FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+ * AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+ * LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+ * OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN
+ * THE SOFTWARE.
+ */
+
+/* midgard.h - definitions for the Midgard shader architecture */
+
+typedef enum
+{
+	midgard_word_type_alu,
+	midgard_word_type_load_store,
+	midgard_word_type_texture,
+	midgard_word_type_unknown
+} midgard_word_type_e;
+
+typedef enum
+{
+	midgard_alu_vmul,
+	midgard_alu_sadd,
+	midgard_alu_smul,
+	midgard_alu_vadd,
+	midgard_alu_lut
+} midgard_alu_e;
+
+/*
+ * ALU words
+ */
+
+typedef enum
+{
+	midgard_alu_op_fadd       = 0x10,
+	midgard_alu_op_fmul       = 0x14,
+	midgard_alu_op_fmin       = 0x28,
+	midgard_alu_op_fmax       = 0x2C,
+	midgard_alu_op_fmov       = 0x30,
+	midgard_alu_op_ffloor     = 0x36,
+	midgard_alu_op_fceil      = 0x37,
+	midgard_alu_op_fdot3      = 0x3C,
+	midgard_alu_op_fdot3r     = 0x3D,
+	midgard_alu_op_fdot4      = 0x3E,
+	midgard_alu_op_freduce    = 0x3F,
+	midgard_alu_op_iadd       = 0x40,
+	midgard_alu_op_isub       = 0x46,
+	midgard_alu_op_imul       = 0x58,
+	midgard_alu_op_imov       = 0x7B,
+	midgard_alu_op_feq        = 0x80,
+	midgard_alu_op_fne        = 0x81,
+	midgard_alu_op_flt        = 0x82,
+	midgard_alu_op_fle        = 0x83,
+	midgard_alu_op_f2i        = 0x99,
+	midgard_alu_op_ieq        = 0xA0,
+	midgard_alu_op_ine        = 0xA1,
+	midgard_alu_op_ilt        = 0xA4,
+	midgard_alu_op_ile        = 0xA5,
+	midgard_alu_op_ball       = 0xA9,
+	midgard_alu_op_bany       = 0xB1,
+	midgard_alu_op_i2f        = 0xB8,
+	midgard_alu_op_csel       = 0xC5,
+	midgard_alu_op_fatan_pt2  = 0xE8,
+	midgard_alu_op_frcp       = 0xF0,
+	midgard_alu_op_frsqrt     = 0xF2,
+	midgard_alu_op_fsqrt      = 0xF3,
+	midgard_alu_op_fexp2      = 0xF4,
+	midgard_alu_op_flog2      = 0xF5,
+	midgard_alu_op_fsin       = 0xF6,
+	midgard_alu_op_fcos       = 0xF7,
+	midgard_alu_op_fatan2_pt1 = 0xF9,
+} midgard_alu_op_e;
+
+typedef enum
+{
+	midgard_outmod_none = 0,
+	midgard_outmod_pos  = 1,
+	midgard_outmod_int  = 2,
+	midgard_outmod_sat  = 3
+} midgard_outmod_e;
+
+typedef enum
+{
+	midgard_reg_mode_half = 1,
+	midgard_reg_mode_full = 2
+} midgard_reg_mode_e;
+
+typedef enum
+{
+	midgard_dest_override_lower = 0,
+	midgard_dest_override_upper = 1,
+	midgard_dest_override_none = 2
+} midgard_dest_override_e;
+
+typedef struct
+__attribute__((__packed__))
+{
+	bool abs         : 1;
+	bool negate      : 1;
+	
+	/* replicate lower half if dest = half, or low/high half selection if
+	 * dest = full
+	 */
+	bool rep_low     : 1;
+	bool rep_high    : 1; /* unused if dest = full */
+	bool half        : 1; /* only matters if dest = full */
+	unsigned swizzle : 8;
+} midgard_vector_alu_src_t;
+
+typedef struct
+__attribute__((__packed__))
+{
+	midgard_alu_op_e op               :  8;
+	midgard_reg_mode_e reg_mode   :  2;
+	unsigned src1 : 13;
+	unsigned src2 : 13;
+	midgard_dest_override_e dest_override : 2;
+	midgard_outmod_e outmod               : 2;
+	unsigned mask                           : 8;
+} midgard_vector_alu_t;
+
+typedef struct
+__attribute__((__packed__))
+{
+	bool abs           : 1;
+	bool negate        : 1;
+	bool full          : 1; /* 0 = half, 1 = full */
+	unsigned component : 3;
+} midgard_scalar_alu_src_t;
+
+typedef struct
+__attribute__((__packed__))
+{
+	midgard_alu_op_e op         :  8;
+	unsigned src1             :  6;
+	unsigned src2             : 11;
+	unsigned unknown          :  1;
+	midgard_outmod_e outmod :  2;
+	bool output_full          :  1;
+	unsigned output_component :  3;
+} midgard_scalar_alu_t;
+
+typedef struct
+__attribute__((__packed__))
+{
+	unsigned src1_reg : 5;
+	unsigned src2_reg : 5;
+	unsigned out_reg  : 5;
+	bool src2_imm     : 1;
+} midgard_reg_info_t;
+
+/*
+ * Load/store words
+ */
+
+typedef enum
+{
+	midgard_op_ld_st_noop   = 0x03,
+	midgard_op_load_attr_16 = 0x95,
+	midgard_op_load_attr_32 = 0x94,
+	midgard_op_load_vary_16 = 0x99,
+	midgard_op_load_vary_32 = 0x98,
+	midgard_op_load_uniform_16 = 0xAC,
+	midgard_op_load_uniform_32 = 0xB0,
+	midgard_op_store_vary_16 = 0xD5,
+	midgard_op_store_vary_32 = 0xD4
+} midgard_load_store_op_e;
+
+typedef struct
+__attribute__((__packed__))
+{
+	midgard_load_store_op_e op : 8;
+	unsigned reg     : 5;
+	unsigned mask    : 4;
+	unsigned swizzle : 8;
+	unsigned unknown : 26;
+	unsigned address : 9;
+} midgard_load_store_word_t;
+
+typedef struct
+__attribute__((__packed__))
+{
+	unsigned type      : 4;
+	unsigned next_type : 4;
+	uint64_t word1     : 60;
+	uint64_t word2     : 60;
+} midgard_load_store_t;
+
